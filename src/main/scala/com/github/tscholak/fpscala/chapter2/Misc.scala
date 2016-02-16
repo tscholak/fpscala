5cbc8d16068d7fc533d51fd923841e603ca8e34f
Torsten Scholak <torsten.scholak@googlemail.com>
2016-02-16 18:10:22 -0500

    added chapter 2

---
+++ src/main/scala/com/github/tscholak/fpscala/chapter2/Misc.scala
@@ -0,0 +1,49 @@
+package com.github.tscholak.fpscala
+
+import scala.annotation.tailrec
+
+object Chapter2 {
+
+  // exercise 2.1
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(n: Int, prev: Int, cur: Int): Int =
+      if (n <= 0) prev
+      else if (n == 1) cur
+      else go(n-1, cur, prev+cur)
+
+    go(n, 0, 1)
+  }
+
+  // exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def go(as: Array[A]): Boolean = {
+      if (as.length > 1) {
+        if (ordered(as(0), as(1))) go(as.drop(1))
+        else false
+      }
+      else true
+    }
+
+    go(as)
+  }
+
+  // exercise 2.3
+  def partial[A,B,C](a: A, f: (A, B) => C): (B => C) = {
+    (b: B) => f(a, b)
+  }
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
+
+  def uncurry[A,B,C](f: A => B => C): ((A, B) => C) = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  // exercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): (A => C) = {
+    (a: A) => f(g(a))
+  }
+
+}
---
+++ src/test/scala/com/github/tscholak/fpscala/chapter2/MiscSpec.scala
@@ -0,0 +1,38 @@
+package com.github.tscholak.fpscala
+
+import org.scalatest.{Matchers, FlatSpec}
+import Chapter2._
+
+class MiscSpec extends FlatSpec with Matchers {
+
+  // exercise 2.1
+  "The fifth Fibonacci number" should "be 3" in {
+    fib(4) should be (3)
+  }
+
+  // exercise 2.2
+  "An Array of 1,2,3,4,5" should "be sorted" in {
+    isSorted(Array(1,2,3,4,5), (a: Int, b: Int) => a < b) should be (true)
+  }
+  "An Array of 1,2,3,5,4" should "not be sorted" in {
+    isSorted(Array(1,2,3,5,4), (a: Int, b: Int) => a < b) should be (false)
+  }
+
+  // exercise 2.3
+  "Partial function evaluation" should "work" in {
+    partial(10, (a: Int, b: Int) => a + b)(20) should be(30)
+  }
+
+  "Currying" should "work" in {
+    curry((a: Int, b: Int) => a + b)(10)(20) should be(30)
+  }
+
+  "Uncurrying" should "work" in {
+    uncurry(curry((a: Int, b: Int) => a + b))(10, 20) should be (30)
+  }
+
+  // exercise 2.5
+  "Function composition" should "work" in {
+    compose((a: Int) => 2 * a, (b: Int) => b + 3)(1) should be (8)
+  }
+}
